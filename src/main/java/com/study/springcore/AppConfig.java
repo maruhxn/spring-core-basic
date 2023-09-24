a213d36e08b0de026259ad03981b0a4d9d7fb949
maruhxn_ <maruhan1016@gmail.com>
2023-09-24 17:28:46 +0900

    Convert to Spring
    스프링 적용

--- src/main/java/com/study/springcore/AppConfig.java
+++ src/main/java/com/study/springcore/AppConfig.java
@@ -7,20 +7,24 @@ import com.study.springcore.member.MemberServiceImpl;
 import com.study.springcore.member.MemoryMemberRepository;
 import com.study.springcore.order.OrderService;
 import com.study.springcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean // @Bean을 통해 스프링 컨테이너에 등록됨.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입.
     }
-
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
